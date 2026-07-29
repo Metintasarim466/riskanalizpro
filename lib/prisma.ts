2571a09238aafda70eebcef6b1c439843a23b1a6
Metintasarim466 <mail-adresin@example.com>
2026-07-29 17:08:52 +0300

    v1.1.0 Paket 1 - Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
